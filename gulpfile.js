93b9ba3f1c0adcdfd1a4297c0a2c918eb3174031
kirk7880@gmail.com <kirk7880@gmail.com>
2014-10-31 13:19:05 -0400

    Added md file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var markdox = require("gulp-markdox");
 var gulp = require('gulp');
+var concat = require("gulp-concat");
 
 gulp.task("doc", function(){
   gulp.src("./lib/*.js")
     .pipe(markdox())
+    .pipe(concat("doc.md"))
     .pipe(gulp.dest("./doc"));
 });
 
