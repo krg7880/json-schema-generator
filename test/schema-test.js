d73b721759b6567c7057b92ba8a1d0af05201b39
kirk7880@gmail.com <kirk7880@gmail.com>
2014-10-31 11:51:56 -0400

    Fixed test

--- test/schema-test.js
+++ test/schema-test.js
@@ -6,8 +6,8 @@ var chai = require('chai');
 chai.use(require('chai-json-schema'));
 
 var expect = chai.expect;
-var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/json.json');
-var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/json.json');
+var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/valid.json');
+var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/valid.json');
 
 data = JSON.parse(data.toString('utf8'))
 schema = JSON.parse(schema.toString('utf8'));
