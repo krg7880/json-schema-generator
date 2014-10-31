8da7e4b8072c9c3f12d6b570431e266ae832c917
kirk7880@gmail.com <kirk7880@gmail.com>
2014-10-31 13:16:45 -0400

    remved unused file

--- gulp.js
+++
@@ -1,7 +0,0 @@
-var markdox = require("gulp-markdox");
-var gulp = require('gulp');
-gulp.task("doc", function(){
-  gulp.src("./lib/*.js")
-    .pipe(markdox())
-    .pipe(gulp.dest("./doc"));
-});
