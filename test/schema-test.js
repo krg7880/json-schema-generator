58929955e7a75ccfb5c17d4ebef3ccc1d42d3caa
Vazha Omanashvili <rainum.ua@gmail.com>
2016-02-18 12:40:01 +0200

    add support for multiple property types in array of objects [#113864785]

--- test/schema-test.js
+++ test/schema-test.js
@@ -11,7 +11,7 @@ var generator = require(path.resolve(process.env.PWD + '/index.js'));
 describe('Generator', function() {
   it('should work with root arrays', function() {
     var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/array.json');
-    data = JSON.parse(data.toString('utf8'))
+    data = JSON.parse(data.toString('utf8'));
 
     var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/array.json');
     schema = JSON.parse(schema.toString('utf8'));
@@ -22,7 +22,7 @@ describe('Generator', function() {
 
   it('should work with root objects', function() {
     var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/object.json');
-    data = JSON.parse(data.toString('utf8'))
+    data = JSON.parse(data.toString('utf8'));
 
     var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/object.json');
     schema = JSON.parse(schema.toString('utf8'));
@@ -33,12 +33,10 @@ describe('Generator', function() {
 
   it('should put the type property first, when possible', function() {
     var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/ordering.json');
-    data = JSON.parse(data.toString('utf8'))
-
+    data = JSON.parse(data.toString('utf8'));
 
     var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/ordering.json');
     schema = JSON.parse(schema.toString('utf8'));
-
 
     var generatedSchema = generator(data);
     expect(JSON.stringify(generatedSchema, null, 4)).to.equal(JSON.stringify(schema, null, 4));
@@ -92,5 +90,16 @@ describe('Generator', function() {
         };
 
     expect(JSON.stringify(generatedSchema, null, 4)).to.equal(JSON.stringify(expectedSchema, null, 4));
+  });
+
+  it('should identify multiple property types in array of objects', function() {
+    var data = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/json/multiple-prop-types.json');
+    data = JSON.parse(data.toString('utf8'));
+
+    var schema = fs.readFileSync(path.resolve(process.env.PWD) + '/test/fixtures/schema/multiple-prop-types.json');
+    schema = JSON.parse(schema.toString('utf8'));
+
+    var generatedSchema = generator(data);
+    expect(generatedSchema).to.deep.equal(schema);
   });
 });
